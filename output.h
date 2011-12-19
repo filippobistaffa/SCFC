fa6c8ef98667188e2b81f5bd43521e2eef6e0e6a
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2011-12-19 01:43:46 +0100

    Document Type Definition for output XML

--- output.h
+++ output.h
@@ -3,7 +3,7 @@
 
 #include "scfc.h"
 
-#define XML_HEADER "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+#define XML_HEADER "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE scf SYSTEM \"http://dl.dropbox.com/u/48064647/output.dtd\">\n"
 #define INDENT(I) {for (i = 0; i < a->level * 2 + 2 + I; i++) fprintf(f, "\t");}
 
 size_t write_xml_output(agent *root, size_t users, char *filename);
